ce49104823f48ffdeea8dc9313edcd0a095ecd6e
yasuyuki <yasuyuki@R5WXKWFNH0noMacBook-Pro.local>
2023-05-04 15:32:00 +0900

    [Add]Eloquentモデル定義

---
+++ database/migrations/2023_05_04_061403_create_likes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('prefecture_id');
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('prefecture_id')
+                ->references('id')
+                ->on('prefectures')
+                ->onDelete('cascade');
+            $table->unique(['user_id', 'prefecture_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+};
